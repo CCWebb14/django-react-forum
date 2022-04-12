ca8a25a231e8f04bece0bc2c06fb4e962ad59832
CCWebb14 <CCWebb14@gmail.com>
2022-04-11 21:29:03 -0700

    removed comments

--- blogapi/posts/serializers.py
+++ blogapi/posts/serializers.py
@@ -3,16 +3,6 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Post
 
-# class CurrentUserDefault(object):
-#     def set_context(self, serializer_field):
-#         self.user_id = serializer_field.context['request'].user.id
-
-#     def __call__(self):
-#         return self.user_id
-
-#     def __repr__(self):
-#         return unicode_to_repr('%s()' % self.__class__.__name__)
-
 class PostSerializer(serializers.ModelSerializer):
 
   author = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
