e3e88e89296a647955fd219082b6eff2d2389d74
CCWebb14 <CCWebb14@gmail.com>
2022-11-04 02:12:09 -0700

    added cooptech to allowed hosts

--- blogapi/config/settings.py
+++ blogapi/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['forum.cooptech.io']
 
 # email verification for final version
 # EMAIL_HOST = 'smtp.sendgrid.net'
