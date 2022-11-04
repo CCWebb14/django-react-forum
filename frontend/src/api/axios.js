ea2814c254749687a392ea8d913ac8571d949308
CCWebb14 <CCWebb14@gmail.com>
2022-11-04 00:13:05 -0700

    altered production base_url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL =
 	process.env.NODE_ENV === 'development'
 		? 'http://localhost:8000/api/v1/'
-		: 'http://localhost:8000/api/v1/';
+		: 'http://cooptech.io:8000/api/v1/';
 
 const app = axios.create({
 	baseURL: BASE_URL,
