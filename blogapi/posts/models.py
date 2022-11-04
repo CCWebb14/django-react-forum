dc750b31d1ad60d8bb40f61679462c40a6eadbdc
CCWebb14 <CCWebb14@gmail.com>
2022-11-04 10:08:15 -0700

    increased titles max character length to 300

--- blogapi/posts/models.py
+++ blogapi/posts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
   author = models.ForeignKey(User, on_delete=models.CASCADE)
-  title = models.CharField(max_length=50)
+  title = models.CharField(max_length=300)
   body = models.TextField()
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
