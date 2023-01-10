9aca720cac1220d89c2c7f60d1c44f39834fcf9d
CCWebb14 <CCWebb14@gmail.com>
2023-01-09 22:34:59 -0800

    changed allowed_hosts to local host for development, created a seperate branch for the hosted version

--- django_rest/config/settings.py
+++ django_rest/config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cooperwebb.xyz']
+ALLOWED_HOSTS = ['localhost']
 
 # email verification for final version
 EMAIL_HOST = 'smtp.sendgrid.net'
